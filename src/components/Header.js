3fe4c03e8729cedd34a9135ee3f5a0dcde5d672f
alyssabao <64719777+alyssabao@users.noreply.github.com>
2020-06-01 13:45:37 +0700

    final fixed netlify error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'C:/Users/inxjv/Desktop/Mojave/movieapp/src/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
